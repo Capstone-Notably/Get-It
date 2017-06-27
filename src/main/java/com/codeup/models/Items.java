7b57af36e6ea33e9ed05d6d5ce8890f44f94e862
roxanavp87 <roxanavp87@gmail.com>
2017-06-27 17:37:24 -0500

    added login, register, fragments and index

---
+++ src/main/java/com/codeup/models/Items.java
@@ -0,0 +1,17 @@
+package com.codeup.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="items")
+public class Items {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String img_url;
+}
