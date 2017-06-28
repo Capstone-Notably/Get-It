363ea2307470c83f295e73a5d16ff0c4791d44e9
roxanavp87 <roxanavp87@gmail.com>
2017-06-27 23:31:19 -0500

    created notably_db

--- src/main/java/com/codeup/models/Items.java
+++ src/main/java/com/codeup/models/Items.java
@@ -14,4 +14,36 @@ public class Items {
 
     @Column(nullable = false)
     private String img_url;
+
+    public Items() {
+    }
+
+    public Items(String name, String img_url) {
+        this.name = name;
+        this.img_url = img_url;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
 }
