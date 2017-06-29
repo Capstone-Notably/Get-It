9fd5058fb06b3f6f5a28f07c35a87e26d9c0ffc9
roxanavp87 <roxanavp87@gmail.com>
2017-06-29 16:14:19 -0500

    Create new item from the view

--- src/main/java/com/codeup/controller/ItemsController.java
+++ src/main/java/com/codeup/controller/ItemsController.java
@@ -1,8 +1,18 @@
 package com.codeup.controller;
 
+import com.codeup.models.Category;
+import com.codeup.models.Item;
 import com.codeup.repositories.ItemsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 /**
  * Created by roxana on 6/28/17.
@@ -11,9 +21,43 @@ import org.springframework.stereotype.Controller;
 public class ItemsController {
     private final ItemsRepository itemsRepository;
 
+    @Value("${items-img-path}")
+    private String itemsImgPath;
+
 
     @Autowired
     public ItemsController(ItemsRepository itemsRepository) {
         this.itemsRepository = itemsRepository;
+    }
+
+    @GetMapping("/items")
+    public String viewItems(@RequestParam("category_id") long category_id, Model model) {
+        Iterable<Item> items = itemsRepository.findByCategory_Id(category_id);
+        model.addAttribute("items", items);
+        return "items/index";
+    }
+
+    @GetMapping("/items/create")
+    public String createCategory(Model model) {
+        model.addAttribute("items", new Item());
+        return "items/create";
+    }
+
+    @PostMapping("/items/create")
+    public String saveCategory(@ModelAttribute Item item, @RequestParam(name = "file") MultipartFile uploadedFile, Model model) {
+        String filename = uploadedFile.getOriginalFilename();
+        String filepath = Paths.get(itemsImgPath, filename).toString();
+        File destinationFile = new File(filepath);
+
+        try {
+            uploadedFile.transferTo(destinationFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+            model.addAttribute("message", "Oops! Something went wrong! " + e);
+        }
+
+        item.setImgUrl(filename);
+        itemsRepository.save(item);
+        return "redirect:/home";
     }
 }
--- src/main/java/com/codeup/repositories/ItemsRepository.java
+++ src/main/java/com/codeup/repositories/ItemsRepository.java
@@ -4,9 +4,12 @@ import com.codeup.models.Item;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by roxana on 6/27/17.
  */
 @Repository
 public interface ItemsRepository  extends CrudRepository<Item, Long>{
+    public Iterable<Item> findByCategory_Id(long category_id);
 }
