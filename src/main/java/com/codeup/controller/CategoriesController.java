4740a59c2007c90ddfb86e2486165bf67448da9a
roxanavp87 <roxanavp87@gmail.com>
2017-06-28 22:46:23 -0500

    view to create a category

--- src/main/java/com/codeup/controller/CategoriesController.java
+++ src/main/java/com/codeup/controller/CategoriesController.java
@@ -3,9 +3,11 @@ package com.codeup.controller;
 import com.codeup.models.Categories;
 import com.codeup.repositories.CategoriesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by roxana on 6/28/17.
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CategoriesController {
     private final CategoriesRepository categoriesRepository;
+
+    @Value("${categories-img-path}")
+    private String categoriesImgPath;
 
     @Autowired
     public CategoriesController(CategoriesRepository categoriesRepository) {
@@ -24,6 +29,19 @@ public class CategoriesController {
         Iterable<Categories> categories = categoriesRepository.findAll();
         model.addAttribute("categories", categories);
         return "testingTables";
+    }
+
+    @GetMapping("/categories/create")
+    public String createCategory(Model model) {
+        Iterable<Categories> categories = categoriesRepository.findAll();
+        model.addAttribute("categories", categories);
+        return "categories/create";
+    }
+
+    @PostMapping("/categories/create")
+    public String saveCategory(Model model) {
+
+        return "resdirect:/home";
     }
 
 }
