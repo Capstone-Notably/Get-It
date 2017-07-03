f83093d4221c46e69c66898e4361a79d4b5e7504
roxanavp87 <roxanavp87@gmail.com>
2017-07-03 18:27:32 -0500

    create entity GroceryList

---
+++ src/main/java/com/codeup/models/GroceryList.java
@@ -0,0 +1,53 @@
+package com.codeup.models;
+
+import javax.persistence.*;
+
+/**
+ * Created by roxana on 7/3/17.
+ */
+@Entity
+@Table(name = "grocery_lists")
+public class GroceryList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public GroceryList() {
+    }
+
+    public GroceryList(String name, User user) {
+        this.name = name;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
