6a631f4be18c2e621dbc243f8038a63f57b84a02
roxanavp87 <roxanavp87@gmail.com>
2017-07-07 11:32:10 -0500

    create methods to find recipes by user_id and recipes_items by recipe_id

--- src/main/java/com/codeup/repositories/RecipeItemsRepository.java
+++ src/main/java/com/codeup/repositories/RecipeItemsRepository.java
@@ -4,9 +4,12 @@ import com.codeup.models.RecipeItem;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by roxana on 7/7/17.
  */
 @Repository
 public interface RecipeItemsRepository extends CrudRepository<RecipeItem, Long> {
+    public List<RecipeItem> findByRecipe_Id(long recipe_id);
 }
--- src/main/java/com/codeup/repositories/RecipesRepository.java
+++ src/main/java/com/codeup/repositories/RecipesRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
 /**
  * Created by roxana on 7/7/17.
  */
 @Repository
 public interface RecipesRepository extends CrudRepository<Recipe, Long> {
+    public List<Recipe> findByUser_Id(long user_id);
 }
