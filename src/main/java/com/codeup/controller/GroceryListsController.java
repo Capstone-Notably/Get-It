51980d2acc4e53a7d5dbe631afd418e9e66ce6be
user <mramirez@usa.com>
2017-07-09 11:20:46 -0500

    Merge branch 'master' of /Users/user/IdeaProjects/Notably with conflicts.

--- src/main/java/com/codeup/controller/GroceryListsController.java
+++ src/main/java/com/codeup/controller/GroceryListsController.java
@@ -16,7 +16,7 @@ import java.util.List;
  * Created by roxana on 7/3/17.
  */
 @Controller
-public class GroceryListsController {
+public class eeeeeGroceryListsController {
     private final GroceryListsRepository groceryListsRepository;
     private final ItemsRepository itemsRepository;
     private final UserItemsRepository userItemsRepository;
