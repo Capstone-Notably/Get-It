138025f42f640bba270a255e47465d511c37a6c8
roxanavp87 <roxanavp87@gmail.com>
2017-07-14 23:38:58 -0500

    fix display name for categories and recipes

--- src/main/java/com/codeup/models/Category.java
+++ src/main/java/com/codeup/models/Category.java
@@ -13,7 +13,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
